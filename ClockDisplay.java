a097ddfcfec2eafb4f49b1e93ce31c3785f871fa
Neo <zidereckn06@gmail.com>
2025-02-07 15:47:11 -0500

    Updated clock.diplay editor = 3:46PM 2/7/25

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -27,7 +27,14 @@ public class ClockDisplay
      *     window = new Rectangle(4,6);
      * 31) creates a new clock set at the time specified by the parameters.
      * 32) Because you're the one setting the time so there's no need to update
-     * 33) 
+     * 33) Printer p1;
+     *     
+     *     p1.print("file1",true);
+     *     
+     *     p2.print("file2",false);
+     *     
+     *     int status = p1.getStatus(3);
+     *     
      * 34) It allows to create 2 squares, 1 triangle, a circle
      * 35) 
      * 36) No
